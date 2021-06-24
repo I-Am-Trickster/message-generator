f35374ef7c04c7e5c4e586b8eb9706ef30d4aeb8
Trickster <78331588+I-Am-Trickster@users.noreply.github.com>
2021-06-24 20:21:18 +0800

    Added comment to menu object

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Object to store output arrays
 const menu = {
     mainCourse: ['fried rice', 'baked rice', 'chicken rice', 'kueh teow', 'singapore mee'],
     drink: ['water', 'ice lemon tea', 'milk oolong tea', 'milo', 'hot chocolate'],
