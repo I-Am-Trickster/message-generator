dbfb23b305c14c598744a2836020770a76df8327
Trickster <78331588+I-Am-Trickster@users.noreply.github.com>
2021-06-24 20:44:37 +0800

    Added for loop to randomly select messages

--- script.js
+++ script.js
@@ -6,9 +6,28 @@ const menu = {
 }
 
 // Store selected menu items in an array
-let outputItems = []
+let outputItems = [];
 
 // Generate random number between 0 and num - 1
 const generateRandomNumber = (num) => {
-    return Math.floor(Math.random() * num)
+    return Math.floor(Math.random() * num);
+}
+
+// Iterate over the object
+for (let item in menu) {
+    let randomIndex = generateRandomNumber(menu[item].length);
+
+    switch (item) {
+        case 'mainCourse':
+            outputItems.push(`For your main course, you should order ${menu[item][randomIndex]}.`);
+            break;
+        case 'drink':
+            outputItems.push(`Feeling thirsty? You should drink ${menu[item][randomIndex]}.`);
+            break;
+        case 'dessert':
+            outputItems.push(`To finish your meal, you should eat ${menu[item][randomIndex]} for dessert.`);
+            break;
+        default:
+            outputItems.push('Unable to choose what you should eat/drink');
+    }
 }
