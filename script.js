84173bf76faea0bb9fce3910f3c4456ede709de4
Trickster <78331588+I-Am-Trickster@users.noreply.github.com>
2021-06-24 18:09:15 +0800

    Created an object with nested arrays to store message components

---
+++ script.js
@@ -0,0 +1,5 @@
+const menu = {
+    mainCourse: ['fried rice', 'baked rice', 'chicken rice', 'kueh teow', 'singapore mee'],
+    drink: ['water', 'ice lemon tea', 'milk oolong tea', 'milo', 'hot chocolate'],
+    dessert: ['ice cream', 'tiramisu', 'chocolate cake', 'apple pie', 'chocolate chip cookie']
+}
